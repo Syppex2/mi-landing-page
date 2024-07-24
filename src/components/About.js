f9b6f480fec315778aef04112b0329e4947782a6
Syppex <167769989+Syppex2@users.noreply.github.com>
2024-07-24 11:25:58 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,17 @@ function About() {
   return (
     <section id="about" className="section">
       <h2>Quiénes Somos</h2>
-      <p>Somos un equipo de profesionales comprometidos con la excelencia. Nuestro objetivo es proporcionar soluciones innovadoras y de alta calidad para nuestros clientes. Estamos orgullosos de ser parte de la comunidad Inacap.</p>
+      <p>
+        Somos un equipo de profesionales comprometidos con la excelencia. Nuestro objetivo es proporcionar soluciones innovadoras y de alta calidad para nuestros clientes. Estamos orgullosos de ser parte de la comunidad {' '}
+        <a 
+          href="https://portales.inacap.cl/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="inacap-link"
+        >
+          Inacap
+        </a>.
+      </p>
     </section>
   );
 }
